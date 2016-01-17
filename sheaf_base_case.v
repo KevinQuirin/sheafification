5085dfb3a44ca8d2d7808848885a94bcd15f951d
KevinQuirin <kevin.quirin@gmail.com>
2016-01-17 20:32:32 +0100

    correct proof of prop 30

--- sheaf_base_case.v
+++ sheaf_base_case.v
@@ -77,3 +77,61 @@ Section Reflective_Subuniverse_base_case.
 
 End Reflective_Subuniverse_base_case.
 
+Section Formalization.
+
+  (** In this section is the proof of Proposition 23, base case of the induction *)
+
+  Context `{ua: Univalence}.
+  Context `{fs: Funext}.
+
+  Let OP := subuniverse_Prop.
+
+  Let helper_trunc0 : forall (T:hSet), IsHSet (T -> subuniverse_Type OP).
+  Proof.
+    intro T. refine (trunc_arrow _).
+    apply subuniverse_Type_is_TruncTypeSn.
+    exact ua.
+  Qed.             
+
+  Let helper_trunc1 : forall (T:hSet) (u: T -> subuniverse_Type OP),
+      IsHSet (O OP (BuildhProp (Trunc (-1) {a:T & u = (λ t, O OP (BuildhProp (a = t)))}))).
+  Proof.
+    intros T u.
+    apply trunc_succ. auto with typeclass_instances.
+  Qed.
+  
+  Definition O0 : hSet -> hSet
+    := λ T:hSet, @BuildhSet ({u: T -> subuniverse_Type OP & O OP (BuildhProp (Trunc (-1) {a:T & u = (λ t, O OP (BuildhProp (a = t)))}))}) (trunc_sigma (helper_trunc0 T) (helper_trunc1 T)).
+
+  Definition OP_to_O0 (P:hProp)
+    : O OP P -> O0 (BuildhSet P).
+  Proof.
+    intro nnp. refine (exist _ _ _).
+    intro p.
+    refine (Build_subuniverse_Type _ _ (BuildhProp Unit) _).
+    revert nnp; apply O_rec; intro p.
+    apply O_unit, tr. exists p.
+    apply path_forall; intro x. cbn in *.
+    apply unique_subuniverse; apply path_trunctype; cbn.
+    apply equiv_iff_hprop.
+    intro tt; apply O_unit; apply path_ishprop.
+    intro t; exact tt.
+  Defined.
+
+  Definition O0_to_OP (P:hProp)
+    : O0 (BuildhSet P) -> O OP P.
+  Proof.
+    intros [u p].
+    revert p; apply O_rec; apply Trunc_rec.
+    intros [a q]. exact (O_unit OP P a).
+  Defined.
+
+  Definition equiv_OP_O0 (P:hProp)
+    : O OP P <~> O0 (BuildhSet P).
+  Proof.
+    refine (equiv_iff_hprop (@OP_to_O0 P) (@O0_to_OP P)).
+    (** Here should be the proof that [O0] defines a modality, combined with [hprop_stability] *)
+  Admitted.
+    
+  
+End Formalization.
